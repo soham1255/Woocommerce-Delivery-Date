f19b043b0de806ffab9cba04faedff0941358797
Soham Patel <39306562+soham1255@users.noreply.github.com>
2023-02-21 22:23:43 +0530

    Update woo-delivery-manager.php

--- woo-delivery-manager.php
+++ woo-delivery-manager.php
@@ -3,45 +3,45 @@
  * Plugin Name: Woo Delivery Manager
  * Plugin URI: https://wordpress.org/plugins/woo-delivery-manager/
  * Description: A flexible and optimized wordpress plugin that allows you to add date & time field on checkout page so user can select the delivery date
- * Author: Soham Patel
+ * Author: Soham Patel | WPSoham
  * Version: 1.0.0
- * Author URI: soham1255@gmail.com
+ * Author URI: sohampatel1202@gmail.com
  */
 
-if( ! defined( 'WPK_PLUGIN_FILE' ) ) :
-	define( 'WPK_PLUGIN_FILE', __FILE__ );
+if( ! defined( 'WPS_PLUGIN_FILE' ) ) :
+	define( 'WPS_PLUGIN_FILE', __FILE__ );
 endif;
 
-if( ! defined( 'WPK_PLUGIN_BASENAME' ) ) :
-	define( 'WPK_PLUGIN_BASENAME', plugin_basename( WPK_PLUGIN_FILE ) );
+if( ! defined( 'WPS_PLUGIN_BASENAME' ) ) :
+	define( 'WPS_PLUGIN_BASENAME', plugin_basename( WPS_PLUGIN_FILE ) );
 endif;
 
-if ( ! defined( 'WPK_WOO_DELIVERY_MANAGER' ) ) :
-	define( 'WPK_WOO_DELIVERY_MANAGER', 'WPK_WOO_DELIVERY_MANAGER' );
+if ( ! defined( 'WPS_WOO_DELIVERY_MANAGER' ) ) :
+	define( 'WPS_WOO_DELIVERY_MANAGER', 'WPS_WOO_DELIVERY_MANAGER' );
 endif;
 
-if ( ! defined( 'WPK_WOO_DELIVERY_MANAGER_VERSION' ) ) :
-	define( 'WPK_WOO_DELIVERY_MANAGER_VERSION', '1.0.0' );
+if ( ! defined( 'WPS_WOO_DELIVERY_MANAGER_VERSION' ) ) :
+	define( 'WPS_WOO_DELIVERY_MANAGER_VERSION', '1.0.0' );
 endif;
 
-if ( ! defined( 'WPK_WOO_DELIVERY_MANAGER_PATH' ) ) :
-	define( 'WPK_WOO_DELIVERY_MANAGER_PATH', plugin_dir_path( __FILE__ ) );
+if ( ! defined( 'WPS_WOO_DELIVERY_MANAGER_PATH' ) ) :
+	define( 'WPS_WOO_DELIVERY_MANAGER_PATH', plugin_dir_path( __FILE__ ) );
 endif;
 
-if ( ! defined( 'WPK_WOO_DELIVERY_MANAGER_URL' ) ) :
-	define( 'WPK_WOO_DELIVERY_MANAGER_URL', plugin_dir_url( __FILE__ ) );
+if ( ! defined( 'WPS_WOO_DELIVERY_MANAGER_URL' ) ) :
+	define( 'WPS_WOO_DELIVERY_MANAGER_URL', plugin_dir_url( __FILE__ ) );
 endif;
 
-if ( ! defined( 'WPK_WOO_DELIVERY_MANAGER_TEXTDOMAIN' ) ) :
-	define( 'WPK_WOO_DELIVERY_MANAGER_TEXTDOMAIN', 'wpk-woo-delivery-manager' );
+if ( ! defined( 'WPS_WOO_DELIVERY_MANAGER_TEXTDOMAIN' ) ) :
+	define( 'WPS_WOO_DELIVERY_MANAGER_TEXTDOMAIN', 'wpk-woo-delivery-manager' );
 endif;
 
 // Manage wordpress backend slider and metabox
 
 
-if ( ! function_exists( 'wpk_delivery_manager_install' ) ) :
+if ( ! function_exists( 'wps_delivery_manager_install' ) ) :
 
-	function wpk_delivery_manager_constructor(){
+	function wps_delivery_manager_constructor(){
 		if( is_admin() ) :
 			require_once "include/class-woo-delivery-manager-admin.php";
 
@@ -54,25 +54,25 @@ if ( ! function_exists( 'wpk_delivery_manager_install' ) ) :
 endif;
 
 
-if ( ! function_exists( 'wpk_delivery_manager_install' ) ) :
+if ( ! function_exists( 'wps_delivery_manager_install' ) ) :
 	
-	function wpk_delivery_manager_install() {
+	function wps_delivery_manager_install() {
 		if ( ! function_exists( 'WC' ) ) :
-			add_action( 'admin_notices', 'wpk_delivery_manager_install_woocommerce_admin_notice' );
+			add_action( 'admin_notices', 'wps_delivery_manager_install_woocommerce_admin_notice' );
 		else :
-			do_action( 'wpk_delivery_manager_init' );
+			do_action( 'wps_delivery_manager_init' );
 		endif;
 	}
-	add_action( 'plugins_loaded', 'wpk_delivery_manager_install', 11 );
-	add_action( 'wpk_delivery_manager_init', 'wpk_delivery_manager_constructor' );
+	add_action( 'plugins_loaded', 'wps_delivery_manager_install', 11 );
+	add_action( 'wps_delivery_manager_init', 'wps_delivery_manager_constructor' );
 endif;
 
 
-if ( ! function_exists( 'wpk_delivery_manager_install_woocommerce_admin_notice' ) ) :
+if ( ! function_exists( 'wps_delivery_manager_install_woocommerce_admin_notice' ) ) :
 	
-	function wpk_delivery_manager_install_woocommerce_admin_notice() {?>
+	function wps_delivery_manager_install_woocommerce_admin_notice() {?>
 		<div class="error">
-			<p><?php echo esc_html( 'WPK Delivery Manager ' . __( 'is enabled but not effective. It requires WooCommerce to work.', WPK_WOO_DELIVERY_MANAGER_TEXTDOMAIN ) ); ?></p>
+			<p><?php echo esc_html( 'WPS Delivery Manager ' . __( 'is enabled but not effective. It requires WooCommerce to work.', WPS_WOO_DELIVERY_MANAGER_TEXTDOMAIN ) ); ?></p>
 		</div>
 		<?php
 	}
